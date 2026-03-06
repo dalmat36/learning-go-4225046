dcb937cae4e41a3d0fea4c4f723158aea9a2774f
Matt Dalesio <matt.dalesio@accuweather.com>
2026-03-06 13:46:27 +0000

    Add go.mod and hello.go files for Go module setup and hello world example

--- Ch01/01_06b/hello.go
+++ Ch01/01_06b/hello.go
@@ -7,3 +7,6 @@ import (
 func main() {
 	fmt.Println("Hello from Go!")
 }
+
+// Build executable file using command: go build hello.go
+// To build with custom name: go build -o custom_name hello.go
