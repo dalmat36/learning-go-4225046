07a1adb4554f5459172ae4445a148e145f257616
Matt Dalesio <matt.dalesio@accuweather.com>
2026-03-06 13:59:03 +0000

    Refactor main function to include string variables and output formatting

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,30 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+// 	02_02b: go run main.go
+// The quick red fox jumped over the lazy brown dog.
+
+	stringLength, err := fmt.Println("The value is", aNumber)
+	if err == nil {
+		fmt.Println("String length:", stringLength)
+	} else {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Printf("The value is: %v\n", aNumber)
+
+	fmt.Printf("Data type: %T\n", aNumber)
+	// https://pkg.go.dev/fmt@go1.26.1
+
+// 02_02b: go run main.go
+// The quick red fox jumped over the lazy brown dog.
+// The value is 42
+// String length: 16
+// The value is: 42
 }
