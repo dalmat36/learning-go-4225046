04fb8ea8ff59dee851d4cabea58f7815c8df36cc
Matt Dalesio <matt.dalesio@accuweather.com>
2026-03-09 17:21:47 +0000

    Add date and time formatting in main function

--- Ch02/02_06b/main.go
+++ Ch02/02_06b/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Printf("Go launched at %s\n", t)
 
-	fmt.Println("Dates and times")
-
+	n := time.Now()
+	fmt.Printf("The time currently is %s\n", n)
+	fmt.Printf("This object's type is %T\n", n)
 }
