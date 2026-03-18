5b5dac97245b6e0b74d81abdd3fc0689612f6f83
Matt Dalesio <matt.dalesio@accuweather.com>
2026-03-18 16:02:26 +0000

    completed store unordered values in maps

--- Ch03/03_05b/main.go
+++ Ch03/03_05b/main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Maps")
+	states := make(map[string]string)
+	states["NY"] = "New York"
+	states["CA"] = "California"
+	states["TX"] = "Texas"
+
+	fmt.Println(states)
+
+	california := states["CA"]
+	fmt.Println(california)
+	delete(states, "TX")
+	fmt.Println(states)
+
+	//print keys and values unordered
+	for k, v := range states {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+
+	// To get the keys in a specific order, we can create a slice of keys and sort it
+	keys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println("\nSorted keys:")
+	// Now we can iterate over the sorted keys to access the values in a specific order
+	for i := range keys {
+		fmt.Println(states[keys[i]])
+	}
 }
