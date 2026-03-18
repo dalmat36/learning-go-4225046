4624894ac37c0ace42d022b96dbb4581f3c88da9
Matt Dalesio <matt.dalesio@accuweather.com>
2026-03-18 15:29:41 +0000

    completed Referfence values with points and store ordered values in arrays

--- Ch03/03_02b/main.go
+++ Ch03/03_02b/main.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	var p *int = &anInt
+	if p == nil {
+		fmt.Println("p is nil")
+	} else {
+		fmt.Println("Value of p:", *p)
+	}
+	
 }
