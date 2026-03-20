3a66e9880b0080c8cfcd2e409fb870127eabf7d2
Matt Dalesio <matt.dalesio@accuweather.com>
2026-03-20 18:20:17 +0000

    completed functions as methods

--- Ch05/05_02b/main.go
+++ Ch05/05_02b/main.go
@@ -6,10 +6,16 @@ import (
 
 func main() {
 	dog := Dog{"Poodle", "Woof"}
-	fmt.Printf("The %v says %v!\n", dog.Breed, dog.Sound)
+	dog.Speak()
+	dog.Sound = "Arf!"
+	dog.Speak()
 }
 
 type Dog struct {
    Breed string
    Sound string
+}
+
+func (d Dog) Speak() {
+	fmt.Printf("The %v says %v!\n", d.Breed, d.Sound)
 }
