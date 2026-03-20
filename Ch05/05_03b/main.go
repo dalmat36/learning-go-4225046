fd34f9ba1f444c97c26af021ca21e719acd2749a
Matt Dalesio <matt.dalesio@accuweather.com>
2026-03-20 18:43:42 +0000

    completed function as goroutine

--- Ch05/05_03b/main.go
+++ Ch05/05_03b/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+//Use go routines when as task would otherwise block the main thread
+//working with local files, making web service api requests, etc
 func main() {
-	fmt.Println("Goroutines")
+	go say("Hello from the Goroutine!")
+	fmt.Println("Hello from main!")
+
+	//declare anonymous function and run as go routine
+	go func(message string) {
+		fmt.Println(message)
+	}("Hello from the anonymous function!")
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("All done!")
+}
+
+func say(message string) {
+	time.Sleep(1 * time.Second)
+	fmt.Println(message)
 }
