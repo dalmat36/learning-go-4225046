f695974f194b49a12042f88ae40385ec482b9ff3
Matt Dalesio <matt.dalesio@accuweather.com>
2026-03-20 15:22:51 +0000

    completed switch statement video

--- Ch04/04_02b/main.go
+++ Ch04/04_02b/main.go
@@ -13,4 +13,32 @@ func main() {
 	dayNumber := int(weekday)
 	fmt.Printf("The day as a number is %v\n", dayNumber)
 
+	var result string
+	dayNumber = 6
+	switch dayNumber {
+	case 1: 
+		result = "It's a Monday"
+	case 2: 
+		result = "It's a Tuesday"
+	case 3: 
+		result = "It's a Wednesday"
+	case 4: 
+		result = "It's a Thursday"
+	case 5: 
+		result = "It's a Friday"
+	default:
+		result = "It's the weekend"
+	}
+	fmt.Println(result)
+
+	x := 42
+	switch {
+	case x < 0:
+		result = "less than zero"
+	case x == 0:
+		result = "equals zero"
+	default:
+		result = "greater than zero"
+	}
+	fmt.Println(result)
 }
